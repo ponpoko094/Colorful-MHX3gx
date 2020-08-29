3aaceb59bb59972e33702206a224f5c6edc1b34c
ponpoko094 <pekepeke094@gmail.com>
2020-08-29 17:28:29 +0900

    [Change] Unicode.h

--- Includes/Unicode.h
+++ Includes/Unicode.h
@@ -2,21 +2,130 @@
 #define UNICODE_H
 
 // HID Symbols
-#define FONT_A      "\uE000" // System Font A button
-#define FONT_B      "\uE001" // System Font B button
-#define FONT_X      "\uE002" // System Font X button
-#define FONT_Y      "\uE003" // System Font Y button
-#define FONT_L      "\uE052" // System Font L button
-#define FONT_R      "\uE053" // System Font R button
-#define FONT_ZL     "\uE054" // System Font ZL button
-#define FONT_ZR     "\uE055" // System Font ZR button
-#define FONT_DU     "\uE079" // System Font D-Pad Up button
-#define FONT_DD     "\uE07A" // System Font D-Pad Down button
-#define FONT_DL     "\uE07B" // System Font D-Pad Left button
-#define FONT_DR     "\uE07C" // System Font D-Pad Right button
-#define FONT_DUD    "\uE07D" // System Font D-Pad Up and Down button
-#define FONT_DLR    "\uE07E" // System Font D-Pad Left and Right button
-#define FONT_CP     "\uE077" // System Font Circle Pad button
-#define FONT_T      "\uE058" // System Font Touch button
+#define FONT_DS_A "\uE000"              // DS Font A button
+#define FONT_DS_B "\uE001"              // DS Font B button
+#define FONT_DS_X "\uE002"              // DS Font X button
+#define FONT_DS_Y "\uE003"              // DS Font Y button
+#define FONT_DS_L "\uE004"              // DS Font L button
+#define FONT_DS_R "\uE005"              // DS Font R button
+#define FONT_DS_CROSSKEY "\uE006"       // DS Font Cross Key
+#define FONT_DS_CLOCK "\uE007"          // DS Font Clock
+#define FONT_DS_SMILEY "\uE008"         // DS Font Smiley
+#define FONT_DS_ANGRY "\uE009"          // DS Font Angry
+#define FONT_DS_Lonely "\uE00A"         // DS Font Lonely
+#define FONT_DS_SLEEPY "\uE00B"         // DS Font Sleepy
+#define FONT_DS_SUNNY "\uE00C"          // DS Font Sunny
+#define FONT_DS_CLOUDY "\uE00D"         // DS Font Cloudy
+#define FONT_DS_RAINY "\uE00E"          // DS Font Rainy
+#define FONT_DS_SNOWY "\uE00F"          // DS Font Snowy
+#define FONT_DS_EXCLAMATION "\uE010"    // DS Font Exclamation
+#define FONT_DS_QUESTION "\uE011"       // DS Font Question
+#define FONT_DS_MAIL "\uE012"           // DS Font Mail
+#define FONT_DS_PHONE "\uE013"          // DS Font Phone
+#define FONT_DS_PLUS "\uE014"           // DS Font Plus
+#define FONT_DS_SPADE "\uE015"          // DS Font Spade
+#define FONT_DS_DIAMOND "\uE016"        // DS Font Diamond
+#define FONT_DS_HEART "\uE017"          // DS Font Heart
+#define FONT_DS_CLOVER "\uE018"         // DS Font Clover
+#define FONT_DS_RIGHT "\uE019"          // DS Font Allow Right
+#define FONT_DS_LEFT "\uE01A"           // DS Font Allow Left
+#define FONT_DS_UP "\uE01B"             // DS Font Allow Up
+#define FONT_DS_DOWN "\uE01C"           // DS Font Allow Down
+#define FONT_DS_REC "\uE01D"            // DS Font Rec
+#define FONT_DS_CAMERA "\uE01E"         // DS Font Camera
+#define FONT_DS_CLOSE1 "\uE01F"         // DS Font Close 1
+#define FONT_DS_LOAD1 "\uE020"          // DS Font Load 1
+#define FONT_DS_LOAD2 "\uE021"          // DS Font Load 2
+#define FONT_DS_LOAD3 "\uE022"          // DS Font Load 3
+#define FONT_DS_LOAD4 "\uE023"          // DS Font Load 4
+#define FONT_DS_LOAD5 "\uE024"          // DS Font Load 5
+#define FONT_DS_LOAD6 "\uE025"          // DS Font Load 6
+#define FONT_DS_LOAD7 "\uE026"          // DS Font Load 7
+#define FONT_DS_LOAD8 "\uE027"          // DS Font Load 8
+#define FONT_DS_CLOSE2 "\uE028"         // DS Font Close 2
+#define FONT_DS_SQUARE_A "\uE029"       // DS Font Square A
+#define FONT_DS_SQUARE_B "\uE02A"       // DS Font Square B
+#define FONT_DS_SQUARE_C "\uE02B"       // DS Font Square C
+#define FONT_DS_SQUARE_D "\uE02C"       // DS Font Square D
+#define FONT_DS_SQUARE_BLACK_A "\uE02D" // DS Font Square Black A
+#define FONT_DS_SQUARE_BLACK_M "\uE02E" // DS Font Square Black M
+#define FONT_DS_CLOSE3 "\uE02F"         // DS Font Square Close 3
+#define FONT_DS_PICTHA_P "\uE030"       // DS Font PICTHA P
+#define FONT_DS_PICTHA_I "\uE031"       // DS Font PICTHA I
+#define FONT_DS_PICTHA_C "\uE032"       // DS Font PICTHA C
+#define FONT_DS_PICTHA_T "\uE033"       // DS Font PICTHA T
+#define FONT_DS_PICTHA_H "\uE034"       // DS Font PICTHA H
+#define FONT_DS_PICTHA_A "\uE035"       // DS Font PICTHA A
+#define FONT_DS_CLOSE4 "\uE036"         // DS Font Close 4
+#define FONT_DS_CLOSE5 "\uE037"         // DS Font Close 5
+#define FONT_DS_CLOSE6 "\uE038"         // DS Font Close 6
+#define FONT_DS_CLOSE7 "\uE039"         // DS Font Close 7
+#define FONT_DS_CLOSE8 "\uE03A"         // DS Font Close 8
+#define FONT_DS_CLOSE9 "\uE03B"         // DS Font Close 9
+#define FONT_DS_CLOSE10 "\uE03C"        // DS Font Close 10
+#define FONT_DS_CLOSE11 "\uE03D"        // DS Font Close 11
+#define FONT_DS_BLACK_CLOSE "\uE03E"    // DS Font Black Close
+#define FONT_DS_WHITE_CLOSE "\uE03F"    // DS Font White Close
+
+#define FONT_WII_REMOTE_POWER "\uE040"      // Wii Font Remote Power button
+#define FONT_WII_REMOTE_CROSSKEY "\uE041"   // Wii Font Remote Cross key
+#define FONT_WII_REMOTE_A "\uE042"          // Wii Font Remote A button
+#define FONT_WII_REMOTE_B "\uE043"          // Wii Font Remote B button
+#define FONT_WII_REMOTE_HOME "\uE044"       // Wii Font Remote Home button
+#define FONT_WII_REMOTE_PLUS "\uE045"       // Wii Font Remote Plus button
+#define FONT_WII_REMOTE_MINUS "\uE046"      // Wii Font Remote Minus button
+#define FONT_WII_REMOTE_1 "\uE047"          // Wii Font Remote 1 button
+#define FONT_WII_REMOTE_2 "\uE048"          // Wii Font Remote 2 button
+#define FONT_WII_NUNCHUK_STICK "\uE049"     // Wii Font Nunchuk Stick
+#define FONT_WII_NUNCHUK_C "\uE04A"         // Wii Font Nunchuk C button
+#define FONT_WII_NUNCHUK_Z "\uE04B"         // Wii Font Nunchuk Z button
+#define FONT_WII_CONTROLLER_A "\uE04C"      // Wii Font Controller A button
+#define FONT_WII_CONTROLLER_B "\uE04D"      // Wii Font Controller B button
+#define FONT_WII_CONTROLLER_X "\uE04E"      // Wii Font Controller X button
+#define FONT_WII_CONTROLLER_Y "\uE04F"      // Wii Font Controller Y button
+#define FONT_WII_CONTROLLER_LSTICK "\uE050" // Wii Font Controller L Stick
+#define FONT_WII_CONTROLLER_RSTICK "\uE051" // Wii Font Controller R Stick
+#define FONT_WII_CONTROLLER_L "\uE052"      // Wii Font Controller L button
+#define FONT_WII_CONTROLLER_R "\uE053"      // Wii Font Controller R button
+#define FONT_WII_CONTROLLER_ZL "\uE054"     // Wii Font Controller ZL button
+#define FONT_WII_CONTROLLER_ZR "\uE055"     // Wii Font Controller ZR button
+#define FONT_WII_RETURN "\uE056"            // Wii Font Return
+#define FONT_WII_SPACE "\uE057"             // Wii Font Space
+#define FONT_WII_CURSOR_POINT "\uE058"      // Wii Font Cursor Point
+#define FONT_WII_CURSOR_POINT_1 "\uE059"    // Wii Font Cursor Point 1
+#define FONT_WII_CURSOR_POINT_2 "\uE05A"    // Wii Font Cursor Point 2
+#define FONT_WII_CURSOR_POINT_3 "\uE05B"    // Wii Font Cursor Point 3
+#define FONT_WII_CURSOR_POINT_4 "\uE05C"    // Wii Font Cursor Point 4
+#define FONT_WII_CURSOR_GRAB "\uE05D"       // Wii Font Cursor Grab
+#define FONT_WII_CURSOR_GRAB_1 "\uE05E"     // Wii Font Cursor Grab 1
+#define FONT_WII_CURSOR_GRAB_2 "\uE05F"     // Wii Font Cursor Grab 2
+#define FONT_WII_CURSOR_GRAB_3 "\uE060"     // Wii Font Cursor Grab 3
+#define FONT_WII_CURSOR_GRAB_4 "\uE061"     // Wii Font Cursor Grab 4
+#define FONT_WII_CURSOR_OPEN "\uE062"       // Wii Font Cursor Open
+#define FONT_WII_CURSOR_OPEN_1 "\uE063"     // Wii Font Cursor Open 1
+#define FONT_WII_CURSOR_OPEN_2 "\uE064"     // Wii Font Cursor Open 2
+#define FONT_WII_CURSOR_OPEN_3 "\uE065"     // Wii Font Cursor Open 3
+#define FONT_WII_CURSOR_OPEN_4 "\uE066"     // Wii Font Cursor Open 4
+#define FONT_WII_MARK "\uE067"              // Wii Font Mark
+#define FONT_WII_MARK_1 "\uE068"            // Wii Font Mark1
+#define FONT_WII_MARK_2 "\uE069"            // Wii Font Mark2
+#define FONT_WII_MARK_3 "\uE06A"            // Wii Font Mark3
+#define FONT_WII_MARK_QUESTION "\uE06B"     // Wii Font Mark Question
+
+#define FONT_3DS_WHITECLOSE "\uE070" // 3DS Font White Close button
+#define FONT_3DS_BLACKCLOSE "\uE071" // 3DS Font Black Close button
+#define FONT_3DS_RETURN "\uE072"     // 3DS Font Return button
+#define FONT_3DS_HOME "\uE073"       // 3DS Font Home button
+#define FONT_3DS_STEP "\uE074"       // 3DS Font Step button
+#define FONT_3DS_GAMECOIN "\uE075"   // 3DS Font Gamecoin button
+#define FONT_3DS_VIDEO "\uE076"      // 3DS Font Video button
+#define FONT_3DS_CP "\uE077"         // 3DS Font Circle Pad button
+#define FONT_3DS_POWER "\uE078"      // 3DS Font Power button
+#define FONT_3DS_DU "\uE079"         // 3DS Font D-Pad Up button
+#define FONT_3DS_DD "\uE07A"         // 3DS Font D-Pad Down button
+#define FONT_3DS_DL "\uE07B"         // 3DS Font D-Pad Left button
+#define FONT_3DS_DR "\uE07C"         // 3DS Font D-Pad Right button
+#define FONT_3DS_DUD "\uE07D"        // 3DS Font D-Pad Up and Down button
+#define FONT_3DS_DLR "\uE07E"        // 3DS Font D-Pad Left and Right button
 
 #endif
