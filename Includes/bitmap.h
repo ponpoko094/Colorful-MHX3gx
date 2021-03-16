17e9f6208213158bd151d9e48d2d1d84dccb185a
ponpoko094 <pekepeke094@gmail.com>
2021-03-16 22:54:35 +0900

    画像表示機能を実装する予定

---
+++ Includes/bitmap.h
@@ -0,0 +1,17 @@
+#ifndef MHX3GX_INCLUDES_PROCESS_PLUS_H_
+#define MHX3GX_INCLUDES_PROCESS_PLUS_H_
+
+namespace CTRPluginFramework {
+
+class Bitmap {
+ public:
+  void Draw();
+  Bitmap();
+  ~Bitmap();
+
+ private:
+};
+
+}  // namespace CTRPluginFramework
+
+#endif  // MHX3GX_INCLUDES_PROCESS_PLUS_H_
