574b2fd37b8a0b7f7e43e199dbb24e1a0f89581d
ponpoko094 <pekepeke094@gmail.com>
2021-03-16 22:55:20 +0900

    Google Style Guideに合わせた

---
+++ Includes/offset.h
@@ -0,0 +1,12 @@
+#ifndef MHX3GX_INCLUDES_OFFSET_H_
+#define MHX3GX_INCLUDES_OFFSET_H_
+
+#include "types.h"
+
+namespace CTRPluginFramework {
+
+static const u32 P_PLAYER_ADDR = 0x8195350;
+
+}  // namespace CTRPluginFramework
+
+#endif  // MHX3GX_INCLUDES_OFFSET_H_
