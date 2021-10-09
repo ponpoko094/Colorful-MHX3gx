27c9e53220d8fe7d2fd9066126cb9241d74fc52c
ponpoko094 <ponkon094n@gmail.com>
2021-10-09 17:42:01 +0900

    Update stdafx.hppに移動

--- Includes/cheats.hpp
+++ Includes/cheats.hpp
@@ -1,36 +1,9 @@
-#ifndef MHX3GX_INCLUDES_CHEATS_H_
-#define MHX3GX_INCLUDES_CHEATS_H
+#ifndef CHEATS_HPP
+#define CHEATS_HPP
 
-#include <3ds.h>
-
-#include <CTRPluginFramework.hpp>
-#include <algorithm>
-#include <chrono>
-#include <cmath>
-#include <cstdarg>
-#include <cstdio>
-#include <cstring>
-#include <ctime>
-#include <fstream>
-#include <istream>
-#include <iterator>
-#include <ostream>
-#include <random>
-#include <sstream>
-#include <string>
-#include <thread>
-#include <vector>
-
-#include "Helpers.hpp"
-#include "csvc.h"
-#include "json.hpp"
-#include "libpon.hpp"
-#include "types.h"
+#include "stdafx.hpp"
 
 namespace CTRPluginFramework {
-
-using CallbackPointer = void (*)(void);
-using StringVector = std::vector<std::string>;
 
 bool IsInQuest();
 
@@ -338,4 +311,4 @@ void BatteryLevel();
 
 }  // namespace CTRPluginFramework
 
-#endif  // MHX3GX_INCLUDES_CHEATS_H
+#endif  // CHEATS_HPP
