91087fe7ba8f4bcbd6ebea41395a6aa95f726428
ponpoko094 <ponkon094n@gmail.com>
2023-01-10 02:24:46 +0900

    refactor functions wrote split to improve maintainability.

--- Sources/team_and_conditions.cpp
+++ Sources/team_and_conditions.cpp
@@ -2,20 +2,23 @@
 
 namespace CTRPluginFramework {
 
-void TeamAndCondition() {
+bool IsMatchTitleId(const std::string& game_title_id) {
   std::string title_id;
+  Process::GetTitleID(title_id);
+  return game_title_id == title_id;
+}
+
+void ReturnNoMatchTitleId() {
   const std::string kMhxTitleId = "0004000000155400";
+  const std::string kOtherGameMessage =
+      "この3gxは日本版MHX(タイトルID:0004000000155400)でのみ動作します。";
+  if (!IsMatchTitleId(kMhxTitleId)) {
+    MessageBox(kOtherGameMessage).SetClear(ClearScreen::Top)();
+    Process::ReturnToHomeMenu();
+  }
+}
 
-  const std::string kSkipSaveFileName = "opskip.bin";
-
-  const std::string kOpeningMessage = "オープニングメッセージ";
-
-  const std::string kThanksMessage =
-      "ぽんぽこ @ponpoko094\n"
-      "の3gxを使用していただき\n"
-      "ありがとうございます。\n"
-      "利用規約の同意をお願いします。";
-
+void TeamAndConditionAgreement() {
   const std::string kTeamAndConditionMessageTitle = "利用規約";
 
   const std::string kTeamAndConditionMessage =
@@ -26,6 +29,18 @@ void TeamAndCondition() {
       "ぽんぽこは一切の責任を負いません。\n"
       "利用規約に同意しますか？";
 
+  const std::string kTeamAndConditionAgreementMessage =
+      "利用規約に同意する必要があります。";
+
+  if (!MessageBox(kTeamAndConditionMessageTitle, kTeamAndConditionMessage,
+                  DialogType::DialogYesNo)
+           .SetClear(ClearScreen::Top)()) {
+    MessageBox(kTeamAndConditionAgreementMessage).SetClear(ClearScreen::Top)();
+    Process::ReturnToHomeMenu();
+  }
+}
+
+void SkipOpeningMessage() {
   const std::string kAskOpeningMessageSkipMessage =
       "利用規約に同意していただき\n"
       "ありがとうございます。\n"
@@ -36,35 +51,34 @@ void TeamAndCondition() {
 
   const std::string kEnjoyPluginMessage = "プラグインを楽しんでください！";
 
-  const std::string kTeamAndConditionAgreementMessage =
-      "利用規約に同意する必要があります。";
-
-  const std::string kOtherGameMessage =
-      "この3gxは日本版MHX(タイトルID:0004000000155400)でのみ動作します。";
-
-  Process::Pause();
-  Process::GetTitleID(title_id);
-  if (title_id != kMhxTitleId) {
-    MessageBox(kOtherGameMessage).SetClear(ClearScreen::Top)();
-    Process::ReturnToHomeMenu();
+  if (MessageBox(kAskOpeningMessageSkipMessage, DialogType::DialogYesNo)
+          .SetClear(ClearScreen::Top)()) {
+    File::Create("opskip.bin");
+    MessageBox(kCreatedOpSkipBinMessage).SetClear(ClearScreen::Top)();
   }
-  if (File::Exists(kSkipSaveFileName) == 1) {
-    return;
-  }
+  MessageBox(kEnjoyPluginMessage).SetClear(ClearScreen::Top)();
+}
+
+void ShowTeamAndCondition() {
+  const std::string kOpeningMessage = "オープニングメッセージ";
+
+  const std::string kThanksMessage =
+      "ぽんぽこ @ponpoko094\n"
+      "の3gxを使用していただき\n"
+      "ありがとうございます。\n"
+      "利用規約の同意をお願いします。";
+
   MessageBox(kOpeningMessage, kThanksMessage).SetClear(ClearScreen::Top)();
-  if (!MessageBox(kTeamAndConditionMessageTitle, kTeamAndConditionMessage,
-                  DialogType::DialogYesNo)
-           .SetClear(ClearScreen::Top)()) {
-    MessageBox(kTeamAndConditionAgreementMessage).SetClear(ClearScreen::Top)();
-    Process::ReturnToHomeMenu();
+  TeamAndConditionAgreement();
+  SkipOpeningMessage();
+}
+
+void TeamAndCondition() {
+  Process::Pause();
+  ReturnNoMatchTitleId();
+  if (File::Exists("opskip.bin") == 0) {
+    ShowTeamAndCondition();
   }
-  if (!MessageBox(kAskOpeningMessageSkipMessage, DialogType::DialogYesNo)
-           .SetClear(ClearScreen::Top)()) {
-    MessageBox(kEnjoyPluginMessage).SetClear(ClearScreen::Top)();
-    return;
-  }
-  File::Create(kSkipSaveFileName);
-  MessageBox(kCreatedOpSkipBinMessage).SetClear(ClearScreen::Top)();
   Process::Play();
 }
 
